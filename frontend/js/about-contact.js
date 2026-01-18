857e96f163af943b1f9949637f3439c88685200f
Mikaz Navodya <mikaznavodya@gmail.com>
2026-01-18 21:26:03 +0530

    Create about-contact.js

---
+++ frontend/js/about-contact.js
@@ -0,0 +1,29 @@
+(function(){
+    emailjs.init("LTmwx-NLagniGqK64"); // replace from EmailJS
+})();
+
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const status = document.getElementById("formStatus");
+    status.style.color = "brown";
+    status.textContent = "Submitting...";
+
+    const params = {
+        from_name: document.getElementById("name").value,
+        from_email: document.getElementById("email").value,
+        pet_type: document.getElementById("pet").value,
+        message: document.getElementById("message").value
+    };
+
+    emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", params)
+        .then(() => {
+            status.style.color = "green";
+            status.textContent = "✅ Message sent successfully!";
+            document.getElementById("contactForm").reset();
+        })
+        .catch(() => {
+            status.style.color = "red";
+            status.textContent = "❌ Failed to send message.";
+        });
+});
